2f206574b2c4c80e0c1ee87e42f80f28d784ac85
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-30 13:02:50 -0800

    Added User repr method

--- model.py
+++ model.py
@@ -23,6 +23,12 @@ class User(db.Model):
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<User user_id=%s email=%s>" % (self.user_id,
+                                               self.email)
+
 
 # Put your Movie and Rating model classes here.
 
