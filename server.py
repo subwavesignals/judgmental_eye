924eb631d9c747c9698050e6a569e8a45a36e3ce
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-31 12:32:03 -0800

    Added comments

--- server.py
+++ server.py
@@ -39,14 +39,21 @@ def user_list():
 def register_form():
     """Displays registration form as GET and handles input as POST"""
 
+    # Return registration form on GET request
     if request.method == "GET":
         return render_template("registration_form.html")
+
+    # Handle registration form submission on POST request
     else:
+
+        # Get form inputs
         email = request.form.get("email")
         password = request.form.get("password")
 
+        # Check if username already in database
         users = User.query.filter_by(email=email).all()
 
+        # If not, add username and redirect to homepage
         if not users:
             user = User(email=email, password=password)
 
@@ -55,6 +62,8 @@ def register_form():
 
             flash("User added.")
             return redirect("/")
+
+        # Else inform user that username already exists
         else:
             flash("User already exists in database.")
             return redirect("/register")
@@ -64,28 +73,39 @@ def register_form():
 def login_form():
     """Display login form as GET and handles input as POST"""
 
+    # Return login form on GET request
     if request.method == "GET":
 
+        # If user is already logged in, inform and redirect to homepage
         if session.get('username') is not None:
             flash("You're already logged in as {}.".format(session.get('username')))
             return redirect("/")
+
+        # Else display login form
         else:
             return render_template("login_form.html")
 
+    # Handle login form submission on POST request
     else:
+
+        # Get form inputs
         email = request.form.get("email")
         password = request.form.get("password")
 
+        # Check to see if username in database, catch sqla error
         try:
             user = User.query.filter_by(email=email).one()
-        except sqlalchemy.orm.exc.NoResultFound:   # specify exception type
+        except sqlalchemy.orm.exc.NoResultFound:
             flash("Username not in database.")
             return redirect("/login")
 
+        # Check for correct password, set session, and redirect to homepage
         if user.password == password:
             session['username'] = user.email
             flash("Login successful.")
             return redirect("/")
+
+        # Inform user of incorrect password
         else:
             flash("Incorrect password.")
             return redirect("/login")
@@ -95,10 +115,14 @@ def login_form():
 def logout():
     """Clear Flask session and log out."""
 
-    session.clear()
-    flash("Logged out.")
-    return redirect("/")
+    # If no user logged in, don't log out; otherwise clear session
+    if session.get('username') is not None:
+        session.clear()
+        flash("Logged out.")
+    else:
+        flash("No user currently logged in.")
 
+    return redirect("/")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
