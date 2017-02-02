aa5e3a6ab76deaa9c962bca66c564f6666a58a2e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-01 17:49:20 -0800

    Added the Judgmental Eye and beratements

--- server.py
+++ server.py
@@ -19,6 +19,14 @@ app.secret_key = "jhdwfjdsufdsjkfdhugsnotdrugsdsubyegd"
 # error.
 app.jinja_env.undefined = StrictUndefined
 
+BERATEMENT_MESSAGES = [
+    "I suppose you don't have such bad taste after all.",
+    "I regret every decision that I've ever made that has brought me to listen to your opinion.",
+    "Words fail me, as your taste in movies has clearly failed you.",
+    "That movie is great. For a clown to watch. Idiot.",
+    "Words cannot express the awfulness of your taste."
+    ]
+
 
 @app.route('/')
 def index():
@@ -79,12 +87,54 @@ def display_movie(movie_id):
             if user:
                 prediction = user.predict_rating(movie)
 
-        print prediction
+        # Either use the prediction or their real rating
+        if prediction:
+
+            # User hasn't scored; use our prediction if we made one
+            effective_rating = prediction
+
+        elif user_has_rated:
+
+            # User has already scored for real; use that
+            effective_rating = user_has_rated.score
+
+        else:
+
+            # User hasn't scored, and we couldn't get a prediction
+            effective_rating = None
+
+        # Get the eye's rating, either by predicting or using real rating
+
+        the_eye = (User.query.filter_by(email="the-eye@of-judgment.com")
+                             .first())
+        eye_rating = Rating.query.filter_by(
+            user_id=the_eye.user_id, movie_id=movie.movie_id).first()
+
+        if eye_rating is None:
+            eye_rating = the_eye.predict_rating(movie)
+
+        else:
+            eye_rating = eye_rating.score
+
+        if eye_rating and effective_rating:
+            difference = abs(eye_rating - effective_rating)
+
+        else:
+            # We couldn't get an eye rating, so we'll skip difference
+            difference = None
+
+        if difference is not None:
+            beratement = BERATEMENT_MESSAGES[int(difference)]
+
+        else:
+            beratement = None
+
         return render_template("movie_details.html",
                                movie=movie,
                                user_has_rated=user_has_rated,
                                average=avg_rating,
-                               prediction=prediction)
+                               prediction=prediction,
+                               beratement=beratement)
 
     # Handle new movie rating on POST request
     else:
